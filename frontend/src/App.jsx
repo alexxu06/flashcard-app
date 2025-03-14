a37a6cbfa9ba5cc5bcb0bb8951bbd704545b038f
alexxu06 <alexxu0308@gmail.com>
2025-03-14 15:12:54 -0700

    initial setup for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './pages/home/HomePage'
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [testThing, setTestThing] = useState("");
+
+    const test = () => {
+        axios.get("api/test")
+        .then(function (response) {
+            console.log(response)
+            setTestThing(response.data.test)
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+    }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        <HomePage />
+        <button onClick={test}>test</button>
+        <p>{testThing}</p>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './HomePage.css'
+
+function HomePage() {
+
+  return (
+    <div className='home-container'>
+        <p>hi</p>
+    </div>
+  )
+}
+
+export default HomePage
