09a538a7dad3014caa00f293102c968b23365f54
destroyer8898 <a.bohaozhang17@gmail.com>
2025-03-14 19:22:08 -0600

    Added NavigationBar component, adjusted App css and jsx
    Added NavigationBar that displays signup/login button and title ("SmartCard"), I also adjusted css for landing page as well as the entire app.

--- frontend/src/pages/landing/LandingPage.jsx
+++ frontend/src/pages/landing/LandingPage.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './LandingPage.css'
+import NavigationBar from '../../components/navigation-bar/NavigationBar'
 
 function LandingPage() {
 
   return (
     <div className='landing-container'>
+        <NavigationBar></NavigationBar>
         <p>This is the landing page</p>
     </div>
   )
