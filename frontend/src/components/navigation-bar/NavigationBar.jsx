041acd144ae4fec61a15a969913697307f542683
alexxu06 <alexxu0308@gmail.com>
2025-03-14 19:24:14 -0700

    added button functionality

--- frontend/src/components/navigation-bar/NavigationBar.jsx
+++ frontend/src/components/navigation-bar/NavigationBar.jsx
@@ -1,14 +1,42 @@
+import { useEffect, useState } from 'react'
 import './NavigationBar.css/'
+import axios from "axios";
+import { useNavigate } from 'react-router'
 
 function NavigationBar() {
+    let navigate = useNavigate();
+    // const [isAuthorized, setIsAuthorized] = useState(null);
+    const [btnLabel, setBtnLabel] = useState("");
 
-  return (
-    <div className='navigation-bar'>
-        <div className='title'>SmartCard</div>
-        <div className='padding'></div>
-        <button className='signup-login-button'>Sign Up / Login</button>
-    </div>
-  )
+    const checkIfAuthenticated = () => {
+        axios.get("api/authentication")
+        .then(function (response) {
+            console.log(response)
+            // setIsAuthorized(true)
+            setBtnLabel("Logout")
+        })
+        .catch(function (error) {
+            console.log(error)
+            // setIsAuthorized(false)
+            setBtnLabel("Sign Up / Login")
+        })
+    }
+
+    useEffect(() => {
+        checkIfAuthenticated()
+    }, [])
+
+    const loginNav = () => {
+        navigate("/login")
+    }
+
+    return (
+        <div className='navigation-bar'>
+            <div className='title'>SmartCard</div>
+            <div className='padding'></div>
+            <button className='signup-login-button' onClick={loginNav}>{btnLabel}</button>
+        </div>
+    )
 }
 
 export default NavigationBar
