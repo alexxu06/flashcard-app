0e44d036c46b419f6df501e4203c1ee072258d5b
alexxu06 <alexxu0308@gmail.com>
2025-03-15 12:47:26 -0700

    added sidebar add functionality

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,25 +1,10 @@
 import { useState } from 'react'
 import './HomePage.css'
-import '../../components/SideBar.css'
-import SideBar from '../../components/SideBar'
-import '../../components/navigation-bar/NavigationBar.css'
+import SideBar from '../../components/sidebar/SideBar'
 import NavigationBar from '../../components/navigation-bar/NavigationBar'
-import './HomePage.css'
 import axios from "axios";
 
 function HomePage() {
-    const [testThing, setTestThing] = useState("");
-
-    const test = () => {
-        axios.get("api/test")
-        .then(function (response) {
-            console.log(response)
-            setTestThing(response.data.test)
-        })
-        .catch(function (error) {
-            console.log(error)
-        })
-    }
 
   return (
     <>
