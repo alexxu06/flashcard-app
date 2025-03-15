b1b34b88f7b6b9b9ca39b652a19e001eb161bef3
alexxu06 <alexxu0308@gmail.com>
2025-03-14 18:31:37 -0700

    added initial routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,24 @@
-from app import app, jwt
+from app import app, jwt, db
 from flask import request, jsonify
 from flask_jwt_extended import *
+from app.models import User, Deck, Flashcard
 
 import json
 
 @app.route("/api/test")
 def test():
     return jsonify({"test": "something"})
+
+@app.route("/api/signup", method=["POST"])
+def signup():
+    return ""
+
+
+@app.route("/api/login", method=["POST"])
+def login():
+    return ""
+
+@app.route("/api/authentication")
+@jwt_required()
+def check_auth():
+    return ""
